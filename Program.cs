2dcb4333626957e3b9cf5383b9fa4f0b9bd03718
Matheus <playermaster10like@gmail.com>
2020-11-04 11:32:50 -0300

    criação de busca de ingredientes ordenados

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using IngredientesExtras.db;
+using System.Linq;
+
+namespace IngredientesExtras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lista com todos os ingredientes do tipo 'Extra' da Hamburgueria:");
+
+            using (var db= new hamburgueriaContext())
+            {
+                var ordem= db.Ingrediente.OrderBy(i=> i.Nome).Where(i=> i.TipoIngredienteId==3);
+                foreach (var ingre in ordem)
+                {
+                   Console.WriteLine($"{ingre.Nome}");
+                }
+            }
+        }
+    }
+}
